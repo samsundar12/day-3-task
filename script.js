5ee02efd3b0840227a6c0d02da46a982676d5062
samsundar12 <139520276+samsundar12@users.noreply.github.com>
2023-11-06 10:00:55 +0530

    demo

---
+++ script.js
@@ -0,0 +1,14 @@
+<style>
+    h1{
+        background-color: pink;
+        color: green;
+        padding-top: 20px;
+        margin-top: 50px;
+        margin-left: 50px;
+        border-color: blue;
+        border-width: 22px;
+        border-style: dashed;
+    }
+
+</style>
+<h1>Hello</h1>
